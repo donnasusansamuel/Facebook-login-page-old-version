8473172aa6a0d7d0db3f4be97ace9db4ff4c8945
donnasusansamuel <donnasamuel95@gmail.com>
2021-07-21 12:11:35 +0530

    fb page

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    $("#signup-form").validate({
+        rules:{
+            fname:{
+                required:true,
+                minlength:4,
+                maxlength:6
+            },
+        sname:{
+            required:true,
+            minlength:4
+        },
+        emailAddress:{
+            required:true,
+            email:true
+        },
+        password:{
+            required:true,
+            minlength:6,
+            
+            maxlength: 30,
+            
+            pwcheck: true  
+        },
+        day:{
+            required:true
+        },
+        month:{
+            required:true
+        },
+        year:{
+            required:true
+        },
+        gender:{
+            required:true
+        }
+    },
+     messages:{
+         fname:{
+            required:"Enter first name",
+            minlength:"Enter atleast 4 characters"
+        },
+        sname:{
+            required:"Enter Surname",
+            minlength:"Enter atleast 4 characters"
+        },
+        password:{
+            required: "Password is not strong enough"
+            
+        }
+
+        
+
+                
+            }
+
+        
+
+        
+    })
+})
